a03457bedf96d44b94e707ba31e11ea1e4e91e9a
arthur-henrique-deliverit <arthur.henrique@deliverit.com.br>
2023-03-21 11:19:47 -0300

    initial commit

---
+++ WhatsApp WebHook/Controller/WebHookController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhatsApp_WebHook.Service;
+
+namespace WhatsApp_WebHook.Controller
+{
+    [ApiController, Route("")]
+    public class WebHookController : ControllerBase
+    {
+        private readonly ILogger<WebHookController> logger;
+        private readonly IWebHookService webHookService;
+
+        public WebHookController(ILogger<WebHookController> logger, IWebHookService webHookService)
+        {
+            this.logger = logger;
+            this.webHookService = webHookService;
+        }
+
+        [HttpGet("webhook")]
+        public string SetupWebHook([FromQuery(Name = "hub_mode")] string hubMode,
+                                          [FromQuery(Name = "hub_challenge")] string hubChallenge,
+                                          [FromQuery(Name = "hub_verify_token")] string hubVerifyToken)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return hubChallenge;
+        } 
+    }
+}
---
+++ WhatsApp WebHook/Service/WebHookService.cs
@@ -0,0 +1,18 @@
+﻿namespace WhatsApp_WebHook.Service
+{
+    public interface IWebHookService
+    {
+    }
+
+    public class WebHookService : IWebHookService
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<WebHookService> logger;
+
+        public WebHookService(IConfiguration configuration, ILogger<WebHookService> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+    }
+}
